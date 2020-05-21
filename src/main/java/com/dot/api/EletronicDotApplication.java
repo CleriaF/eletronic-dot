f579fed0c25ce3c899d45a51ecfd348028749b55
cleriafreitas <cleriatrb@gmail.com>
2020-05-20 22:28:22 -0300

    Arquivos iniciais do projeto.

---
+++ src/main/java/com/dot/api/EletronicDotApplication.java
@@ -0,0 +1,13 @@
+package com.dot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicDotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicDotApplication.class, args);
+	}
+
+}
